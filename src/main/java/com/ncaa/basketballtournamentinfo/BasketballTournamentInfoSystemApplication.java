b473bbd80e42437449ac3df3d6aff7c255a03679
arperez22 <aperez21@huskers.unl.edu>
2025-08-31 17:05:00 -0500

    Initial Commit

---
+++ src/main/java/com/ncaa/basketballtournamentinfo/BasketballTournamentInfoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ncaa.basketballtournamentinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballTournamentInfoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballTournamentInfoSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncaa/basketballtournamentinfo/BasketballTournamentInfoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncaa.basketballtournamentinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballTournamentInfoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
