a5ada01fe9a8cda92263b2b34eb373cb29a0d965
arperez22 <aperez21@huskers.unl.edu>
2025-09-01 22:13:38 -0500

    Created entities for database tables

---
+++ src/main/java/com/ncaa/basketballtournamentinfo/player/Player.java
@@ -0,0 +1,133 @@
+package com.ncaa.basketballtournamentinfo.player;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name="players")
+public class Player {
+    // TODO: Implement Foreign Key (Relational Mapping)
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long playerId;
+    private String name;
+    private Integer jerseyNumber;
+    @Column(name="class")
+    private String year;
+    private String position;
+    private String height;
+    private Double weight;
+    private String hometown;
+    private String highSchool;
+
+    public Player() {}
+
+    public Player(String name, Integer jerseyNumber, String year, String position, String height,
+                  Double weight, String hometown, String highSchool) {
+
+        this.name = name;
+        this.jerseyNumber = jerseyNumber;
+        this.year = year;
+        this.position = position;
+        this.height = height;
+        this.weight = weight;
+        this.hometown = hometown;
+        this.highSchool = highSchool;
+    }
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public String getHighSchool() {
+        return highSchool;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setJerseyNumber(Integer jerseyNumber) {
+        this.jerseyNumber = jerseyNumber;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public void setHighSchool(String highSchool) {
+        this.highSchool = highSchool;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(getPlayerId(), player.getPlayerId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getPlayerId());
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "playerId=" + playerId +
+                ", name='" + name + '\'' +
+                ", jerseyNumber=" + jerseyNumber +
+                ", year='" + year + '\'' +
+                ", position='" + position + '\'' +
+                ", height='" + height + '\'' +
+                ", weight=" + weight +
+                ", hometown='" + hometown + '\'' +
+                ", highSchool='" + highSchool + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/ncaa/basketballtournamentinfo/team/Team.java
@@ -0,0 +1,168 @@
+package com.ncaa.basketballtournamentinfo.team;
+
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Objects;
+
+
+@Entity
+@Table(name="teams")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String record;
+    private Integer wins;
+    private Integer losses;
+    private Integer conferenceWins;
+    private Integer conferenceLosses;
+    private String university;
+    private String coach;
+    private String conference;
+    private String location;
+    private String nickname;
+
+    public Team(String name, String record, Integer wins, Integer losses, Integer conferenceWins,
+                Integer conferenceLosses, String university, String coach, String conference,
+                String location, String nickname) {
+
+        this.name = name;
+        this.record = record;
+        this.wins = wins;
+        this.losses = losses;
+        this.conferenceWins = conferenceWins;
+        this.conferenceLosses = conferenceLosses;
+        this.university = university;
+        this.coach = coach;
+        this.conference = conference;
+        this.location = location;
+        this.nickname = nickname;
+    }
+
+    public Team() {
+
+    }
+
+    public Long getId() { return id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRecord() {
+        return record;
+    }
+
+    public Integer getWins() {
+        return wins;
+    }
+
+    public Integer getLosses() {
+        return losses;
+    }
+
+    public Integer getConferenceWins() {
+        return conferenceWins;
+    }
+
+    public Integer getConferenceLosses() {
+        return conferenceLosses;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public String getCoach() {
+        return coach;
+    }
+
+    public String getConference() {
+        return conference;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+
+    public void setWins(Integer wins) {
+        this.wins = wins;
+    }
+
+    public void setLosses(Integer losses) {
+        this.losses = losses;
+    }
+
+    public void setConferenceWins(Integer conferenceWins) {
+        this.conferenceWins = conferenceWins;
+    }
+
+    public void setConferenceLosses(Integer conferenceLosses) {
+        this.conferenceLosses = conferenceLosses;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public void setCoach(String coach) {
+        this.coach = coach;
+    }
+
+    public void setConference(String conference) {
+        this.conference = conference;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return Objects.equals(getId(), team.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", record='" + record + '\'' +
+                ", wins=" + wins +
+                ", losses=" + losses +
+                ", conferenceWins=" + conferenceWins +
+                ", conferenceLosses=" + conferenceLosses +
+                ", university='" + university + '\'' +
+                ", coach='" + coach + '\'' +
+                ", conference='" + conference + '\'' +
+                ", location='" + location + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
+}
